6cd4ef068897c76fd0da531350e95dc0153ea8b4
Jakob Löw <jakob@m4gnus.de>
2024-06-26 10:35:37 +0200

    :sparkles: update publication page generation to (correctly) incorperate entries which have no links

--- generate-papers-page.py
+++ generate-papers-page.py
@@ -17,6 +17,14 @@ research group, ordered by date descending.
 
 """
 
+conference_head = """
+## Posters, Talks and more
+
+The following is a list of conference talks, posters and other research
+oriented contributions by members of our research group.
+
+"""
+
 # generate this using `cat content/_index.md | grep -Eo 'orcid.org/[0-9A-Z-]+'`
 orcids = [
     '0000-0002-6930-9271',
@@ -33,7 +41,7 @@ orcids = [
 #orcid_auth = OrcidAuthentication(client_id=client_id, client_secret=client_secret)
 #access_token = orcid_auth.get_public_access_token()
 
-known_urls = []
+known_works = []
 papers = []
 for orcid in orcids:
     #orcid = Orcid(orcid_id=orcid, orcid_access_token=access_token, state="public")
@@ -53,9 +61,14 @@ for orcid in orcids:
             print(f"could not parse date: {date_split} for {work['title']}", file=sys.stderr)
             continue
 
-        if work["url"] in known_urls:
+        if work['url'] in known_works or work['title'] in known_works:
             continue
-        known_urls.append(work["url"])
+
+        if work['url'] is None:
+            known_works.append(work['title'])
+            print(work)
+        else:
+            known_works.append(work["url"])
 
         papers.append({
             "order": year * 100 + month,
@@ -69,5 +82,14 @@ papers.sort(key=lambda x: -x["order"])
 with open("content/publications.md", "w+") as fd:
     fd.write(page_head)
     for paper in papers:
-        fd.write(f"- {paper['date']}: [{paper['title']}]({paper['url']})\n")
+        if paper['url']:
+            fd.write(f"- {paper['date']}: [{paper['title']}]({paper['url']})\n")
+
+    fd.write("\n")
+    fd.write(conference_head)
+
+    for paper in papers:
+        if not paper['url']:
+            fd.write(f"- {paper['date']}: {paper['title']}\n")
+
     fd.write("\n")
